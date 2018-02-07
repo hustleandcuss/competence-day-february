1570161976a0a85529d8e74809b30ac91098be36
Olivia Lennerö <olivialennero@gmail.com>
2018-02-07 20:55:38 +0100

    Adds objects list

---
+++ CompetenceDay/Objects.swift
@@ -0,0 +1,11 @@
+//
+//  Objects.swift
+//  CompetenceDay
+//
+//  Created by Olivia Lennerö on 2018-02-05.
+//  Copyright © 2018 Olivia Lennerö. All rights reserved.
+//
+
+struct Objects {
+    let objects = ["computer keyboard", "beer bottle", "balloon", "coffee mug", "digital watch", "laptop", "computer mouse"]
+}
